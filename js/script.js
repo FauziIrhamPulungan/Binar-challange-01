29dd1d0ce397f3ac563b61b78a7297d4c168b4b9
FauziIrhamPulungan <fauzyirham24@gmail.com>
2022-08-13 21:35:49 +0700

    done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".carouse-content", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    autoHeight: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
